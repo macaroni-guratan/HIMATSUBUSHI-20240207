9073d6cd05f6cc375df2cec3b14fda8d5eba625a
macaroni-guratan <mtmrkut@gmail.com>
2024-02-08 01:58:51 +0900

    test10

--- rabbit/scripts/start.sh
+++ rabbit/scripts/start.sh
@@ -21,6 +21,5 @@ php artisan passport:client --personal --name=sorcial;
 php artisan passport:install;
 php artisan optimize;
 php artisan route:list;
-
-npm run dev;
-php artisan serve --host=0.0.0.0 --port=8080;
+(npm run dev;) &
+(php artisan serve --host=0.0.0.0 --port=8080) &;
