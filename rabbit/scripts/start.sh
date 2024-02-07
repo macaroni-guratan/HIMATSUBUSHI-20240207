a09d8c59f07cbedf6222b1c637bda3bc487d2a6d
macaroni-guratan <mtmrkut@gmail.com>
2024-02-08 02:45:32 +0900

    fix

--- rabbit/scripts/start.sh
+++ rabbit/scripts/start.sh
@@ -8,4 +8,9 @@ chmod 777 ./database/database.sqlite
 echo "Running migrations..."
 php artisan migrate --seed --force;
 
+php artisan passport:client --password --name=password --provider=users;
+php artisan passport:client --personal --name=sorcial;
+php artisan passport:install;
+php artisan optimize;
+
 php artisan serve --host=0.0.0.0 --port=8080 & npm run dev;
