d7560604e7475fe0048d9eb1abbb7e5e461476d4
macaroni-guratan <mtmrkut@gmail.com>
2024-02-08 02:41:34 +0900

    fix

--- rabbit/scripts/start.sh
+++ rabbit/scripts/start.sh
@@ -1,1 +1,11 @@
+#DB
+echo "clean database.sqlite file"
+rm ./database/database.sqlite
+echo "create database.sqlite file"
+touch ./database/database.sqlite;
+echo "change op permission database.sqlite file"
+chmod 777 ./database/database.sqlite
+echo "Running migrations..."
+php artisan migrate --seed --force;
+
 php artisan serve --host=0.0.0.0 --port=8080 & npm run dev;
