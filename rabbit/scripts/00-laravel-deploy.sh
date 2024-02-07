7755815a445f3c7b83e437b694e7d87ea979e20c
macaroni-guratan <mtmrkut@gmail.com>
2024-02-08 01:33:31 +0900

    env update

--- rabbit/scripts/00-laravel-deploy.sh
+++ rabbit/scripts/00-laravel-deploy.sh
@@ -5,8 +5,8 @@ echo "Running composer"
 # composer global require hirak/prestissimo
 composer install --no-dev --working-dir=/var/www/html
 
-echo "copy .env..."
-cp /etc/secrets/.env ./
+# echo "copy .env..."
+# cp /etc/secrets/.env ./
 
 echo "Caching config..."
 php artisan config:cache
