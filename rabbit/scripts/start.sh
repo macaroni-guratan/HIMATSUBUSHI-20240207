1c722dff85e79128bd48f9a460694906785ceb0b
macaroni-guratan <mtmrkut@gmail.com>
2024-02-08 23:31:13 +0900

    typo

--- rabbit/scripts/start.sh
+++ rabbit/scripts/start.sh
@@ -16,4 +16,4 @@ php artisan passport:client --personal --name=sorcial;
 php artisan passport:install;
 php artisan optimize;
 
-npm run dev & artisan serve --host=0.0.0.0 --port=8080;
+npm run dev & php artisan serve --host=0.0.0.0 --port=8080;
