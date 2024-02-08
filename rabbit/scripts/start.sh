e29f18622c51c8fffe4888b6219a26f8438725f8
macaroni-guratan <mtmrkut@gmail.com>
2024-02-09 00:04:18 +0900

    dockerfile test3 fix3

--- rabbit/scripts/start.sh
+++ rabbit/scripts/start.sh
@@ -1,3 +1,6 @@
+RUN yarn install
+RUN npm run build
+
 #ENV
 echo "copy .env..."
 cp /etc/secrets/.env ./
@@ -16,4 +19,5 @@ php artisan passport:client --personal --name=sorcial;
 php artisan passport:install;
 php artisan optimize;
 
-npm run dev & php artisan serve --host=0.0.0.0 --port=8080;
+# npm run dev & php artisan serve --host=0.0.0.0 --port=8080;
+php artisan serve --host=0.0.0.0 --port=8080;
